1ece8debbb3196581dfab4cab785cf8b20a82443
Dheeraj Kumar Thakur <dk35118@gmail.com>
2026-02-12 13:50:15 +0530

    changes

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//  export const API_BASE_URL = "https://project1-backend-4u0d.onrender.com/api";
- export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL = "https://project1-backend-4u0d.onrender.com/api";
+// export const API_BASE_URL = "http://localhost:5000/api";
 //export const API_BASE_URL = (window.location.hostname === "localhost") ? "https://project1-backend-4u0d.onrender.com/api" : "https://project1-backend-4u0d.onrender.com/api";
 
 const api = axios.create({
